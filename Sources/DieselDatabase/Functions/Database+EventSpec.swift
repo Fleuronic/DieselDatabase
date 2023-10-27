e8b084bfe79f628dfe889dbd6aa503ecda0722cb
Jordan Kay <jordan@fleuronic.com>
2023-10-27 10:26:50 -0400

    Update dependencies

--- Sources/DieselDatabase/Functions/Database+EventSpec.swift
+++ Sources/DieselDatabase/Functions/Database+EventSpec.swift
@@ -1,6 +1,7 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
 import struct Diesel.Event
+import struct Diesel.Show
 import struct Diesel.Location
 import struct DieselService.EventBaseFields
 import struct DieselService.EventListFields
@@ -8,6 +9,7 @@ import struct DieselService.EventDetailsFields
 import struct DieselService.VenueBaseFields
 import struct DieselService.AddressBaseFields
 import struct DieselService.LocationBaseFields
+import struct DieselService.ShowBaseFields
 import protocol DieselService.EventSpec
 import protocol Catenoid.Database
 
@@ -34,14 +36,21 @@ extension Database: EventSpec {
 				let location = Location.Identified(
 					fields: await fetch(LocationBaseFields.self, with: fields.locationID).value!
 				)
-
+				
+				let show = await fields.showID.asyncMap { id in
+					Show.Identified(
+						fields: await fetch(ShowBaseFields.self, with: id).value!
+					)
+				}
+				
 				return await insert(
 					Event.Identified(
 						fields: fields,
+						show: show,
 						location: location,
 						venue: fields.venueID.asyncMap { id in
 							let venueFields = await fetch(VenueBaseFields.self, with: id).value!
-
+							
 							return .init(
 								fields: venueFields,
 								address: .init(
